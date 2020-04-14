a353667ec0458eaeb57b271f04b68d7d0da3dee4
AleCava01 <alessandro.cavalieri5@outlook.it>
2020-04-15 00:24:57 +0200

    15.4

--- Pages/homepage.php
+++ Pages/homepage.php
@@ -3,5 +3,8 @@
         <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
     </head>
     <body>
+        <?php
+        include "navbar.php";
+        ?>
     </body>
 </html>
---
+++ Pages/navbar.php
@@ -0,0 +1,5 @@
+<nav>
+    <ul>
+        <li><a href="#">sdsf</a></li>
+    </ul>
+</nav> 
