57b95121c7bc5853453883d2491e3f9e9652fa1d
AleCava01 <alessandro.cavalieri5@outlook.it>
2020-05-17 01:18:41 +0200

    film info + video buffer

--- Pages/homepage.php
+++ Pages/homepage.php
@@ -7,7 +7,9 @@
         <link rel="icon" href="Images/icon.png">
         <link rel="stylesheet" href="OwlCarousel/dist/assets/owl.carousel.min.css">
         <link rel="stylesheet" href="OwlCarousel/dist/assets/owl.theme.default.min.css">
-
+        <link href='https://vjs.zencdn.net/7.7.6/video-js.css' rel='stylesheet' />
+        <script src='https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js'></script>
+        <link href='https://unpkg.com/video.js@7/dist/video-js.min.css'rel='stylesheet'/>
 
 
     </head>
@@ -127,15 +129,42 @@
                     echo("<div class='div-".$film["id_f"]."-desc film-info-div' id='div-".$film["id_f"]."-desc' style='background: linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url(".$film["copertina"]."); background-repeat:no-repeat;background-position: center center; background-size:cover;'>");
                     echo("<div>");
                     echo("<h1 class='film-info-title'>".$film["titolo"]."</h1>");
-
+                    echo("<hr class='separator'>");
                     echo("</div>");
 
                     echo("<div class='row' style='height:85%'>");
-                    echo("<div class='col-9'>");
-                    echo("<h5 class='film-info-semi-title'>Descrizione</h5>");
+                    echo("<div class='col-5'>");
+                    echo("<h4 class='film-info-semi-title-top'>Descrizione</h4>");
                     echo("<p class='film-info-desc-p'>".$film["descrizione"]."</p>");
-                    echo("<h5 class='film-info-semi-title'>Regista</h5>");
+                    echo("<h4 class='film-info-semi-title'>Regista</h4>");
                     echo("<p class='film-info-reg-p'>".$film["regista"]."</p>");
+                    echo("<h4 class='film-info-semi-title'>Durata</h4>");
+                    echo("<p class='film-info-reg-p'>".(int)((int)$film["durata"]/60)." ore e ".((int)$film["durata"]%60)." minuti</p>");
+                    echo("<a href='video_buffer.php?id_f=".$film["id_f"]."' class='btn btn-secondary' style='width:10%; background-color:white; color:black'>Play</a>");
+                    echo("</div>");
+                    echo("<div class='col-4'>");
+                    echo("<h4 class='film-info-semi-title-top'>Trailer</h4>");
+
+                    echo("<video
+                    id='my-video'
+                    class='video-js vjs-theme-city'
+                    controls
+                    preload='auto'
+                    width='700'
+                    height='393'
+                    poster='Images/trailer.png'
+                    data-setup='{}'
+                  >
+                    <source src='".$film["trailer_URI"]."' type='video/mp4' />
+                    <source src='".$film["trailer_URI"]."' type='video/webm' />
+                    <p class='vjs-no-js'>
+                      To view this video please enable JavaScript, and consider upgrading to a
+                      web browser that
+                      <a href='https://videojs.com/html5-video-support/' target='_blank'
+                        >supports HTML5 video</a
+                      >
+                    </p>
+                  </video>");
                     echo("</div>");
                     echo("<div class='col-3'>");
                     echo("<div class='locandina-info-wrapper'>");
@@ -160,6 +189,7 @@
             <script src="smooth-scroll-master/dist/smooth-scroll.polyfills.min.js"></script>
             <script src="jquery-mousewheel/jquery.mousewheel.min.js"></script>
             <script>var scroll = new SmoothScroll('a[href*="#"]');</script>
+            <script src='https://vjs.zencdn.net/7.7.6/video.js'></script>
             <script src="OwlCarousel/dist/owl.carousel.min.js"></script>
             <script src="Scripts/owl-carousels.js"></script>        
             <script src="Scripts/category-toggler.js"></script>
--- Pages/video_buffer.php
+++ Pages/video_buffer.php
@@ -1,133 +1,4 @@
-<div style="width:100%; height:100%">
-<link rel="icon" href="Images/icon.png">
-
 <?php
-
-/**
- * Description of VideoStream
- *
- * @author Rana
- * @link http://codesamplez.com/programming/php-html5-video-streaming-tutorial
- */
-class VideoStream
-{
-    private $path = "";
-    private $stream = "";
-    private $buffer = 102400;
-    private $start  = -1;
-    private $end    = -1;
-    private $size   = 0;
- 
-    function __construct($filePath) 
-    {
-        $this->path = $filePath;
-    }
-     
-    /**
-     * Open stream
-     */
-    private function open()
-    {
-        if (!($this->stream = fopen($this->path, 'rb'))) {
-            die('Could not open stream for reading');
-        }
-         
-    }
-     
-    /**
-     * Set proper header to serve the video content
-     */
-    private function setHeader()
-    {
-        ob_get_clean();
-        header("Content-Type: video/mp4");
-        header("Cache-Control: max-age=2592000, public");
-        header("Expires: ".gmdate('D, d M Y H:i:s', time()+2592000) . ' GMT');
-        header("Last-Modified: ".gmdate('D, d M Y H:i:s', @filemtime($this->path)) . ' GMT' );
-        $this->start = 0;
-        $this->size  = filesize($this->path);
-        $this->end   = $this->size - 1;
-        header("Accept-Ranges: 0-".$this->end);
-         
-        if (isset($_SERVER['HTTP_RANGE'])) {
-  
-            $c_start = $this->start;
-            $c_end = $this->end;
- 
-            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
-            if (strpos($range, ',') !== false) {
-                header('HTTP/1.1 416 Requested Range Not Satisfiable');
-                header("Content-Range: bytes $this->start-$this->end/$this->size");
-                exit;
-            }
-            if ($range == '-') {
-                $c_start = $this->size - substr($range, 1);
-            }else{
-                $range = explode('-', $range);
-                $c_start = $range[0];
-                 
-                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $c_end;
-            }
-            $c_end = ($c_end > $this->end) ? $this->end : $c_end;
-            if ($c_start > $c_end || $c_start > $this->size - 1 || $c_end >= $this->size) {
-                header('HTTP/1.1 416 Requested Range Not Satisfiable');
-                header("Content-Range: bytes $this->start-$this->end/$this->size");
-                exit;
-            }
-            $this->start = $c_start;
-            $this->end = $c_end;
-            $length = $this->end - $this->start + 1;
-            fseek($this->stream, $this->start);
-            header('HTTP/1.1 206 Partial Content');
-            header("Content-Length: ".$length);
-            header("Content-Range: bytes $this->start-$this->end/".$this->size);
-        }
-        else
-        {
-            header("Content-Length: ".$this->size);
-        }  
-         
-    }
-    
-    /**
-     * close curretly opened stream
-     */
-    private function end()
-    {
-        fclose($this->stream);
-        exit;
-    }
-     
-    /**
-     * perform the streaming of calculated range
-     */
-    private function stream()
-    {
-        $i = $this->start;
-        set_time_limit(0);
-        while(!feof($this->stream) && $i <= $this->end) {
-            $bytesToRead = $this->buffer;
-            if(($i+$bytesToRead) > $this->end) {
-                $bytesToRead = $this->end - $i + 1;
-            }
-            $data = fread($this->stream, $bytesToRead);
-            echo $data;
-            flush();
-            $i += $bytesToRead;
-        }
-    }
-     
-    /**
-     * Start streaming video content
-     */
-    function start()
-    {
-        $this->open();
-        $this->setHeader();
-        $this->stream();
-        $this->end();
-    }
-}
 include "DBsettings.php";
 include "logcontrol.php";
 
@@ -140,8 +11,86 @@ if((mysqli_fetch_assoc($conn->query("SELECT EXISTS(SELECT * FROM lista WHERE id_
 
 
 $filmpath = mysqli_fetch_assoc($conn->query("SELECT URI from film where id_f = ".$id_f.";"))["URI"];
-$stream = new VideoStream($filmpath);
 session_write_close();
-$stream->start();
 ?>
 </div>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="CSS/navbar2.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="icon" href="Images/icon.png">
+    <link href="https://vjs.zencdn.net/7.7.6/video-js.css" rel="stylesheet" />
+    <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+    <link href="https://unpkg.com/video.js@7/dist/video-js.min.css"rel="stylesheet"/>
+    <style>
+        *{overflow:hidden;outline:none;}
+        nav{
+            background-color:black;
+        }
+        body{
+            background-color:black;
+        }
+        .rate-div{
+            background-color :red;
+            position:absolute;
+            top:0;
+            left:0;
+            right:0;
+            bottom:0;
+            height:100%;
+            width:100%;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a href="#" onclick="history.back();return false;" class="nav-link active"><i class="fas fa-arrow-left"></i></a>
+            </li>
+        </ul>
+        <a class="nav-link navbar-brand active mr-auto" href="homepage.php"><img src="Images/logo.png" width="120px"></a>   
+    </nav>
+    <div class="rate-div" id="rate-div">
+    <video
+        id="my-video"
+        class="video-js vjs-theme-city"
+        controls
+        preload="auto"
+        width="2560"
+        height="1300"
+        poster=""
+        data-setup="{}"
+    >
+    <?php 
+    echo("<source src='".$filmpath."' type='video/mp4'/>");
+    echo("<source src='".$filmpath."' type='video/webm'/>");
+    ?>
+        <p class="vjs-no-js">
+        To view this video please enable JavaScript, and consider upgrading to a
+        web browser that
+        <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+        </p>
+    </video>
+    </div>
+    <script src="https://vjs.zencdn.net/7.7.6/video.js"></script>
+    <script src="node_modules/videojs-hls-quality-selector/dist/videojs-hls-quality-selector.min.js"></script>
+    <script>
+    document.getElementById("my-video").setAttribute('width',document.documentElement.clientWidth);
+    document.getElementById("my-video").setAttribute('height',document.documentElement.clientHeight-60);	
+    var player= videojs('#my-video');
+    player.autoplay(true);
+    var ended = player.ended();
+ 
+    player.ready(function(){
+        this.on("ended", function(){  
+
+        document.getElementById("my-video").style.setProperty("display", "none", "important");
+
+        });
+    });      
+    </script> 
+</body>
+
