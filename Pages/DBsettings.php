5388027c5a2078e0a1265c3daa9cd9fabc4df74c
Alessandro Tobia Cavalieri <52034469+AleCava01@users.noreply.github.com>
2023-06-30 18:00:34 +0200

    Update DBsettings.php

--- Pages/DBsettings.php
+++ Pages/DBsettings.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "35.238.68.198";
+$db_host = "localhost";
 $db_user = "root";
-$db_password = "HITICtioNtro1";
-$db_name = "FilmAdvisor";
+$db_password = "";
+$db_name = "filmadvisor";
 $conn = new mysqli($db_host, $db_user, $db_password);
 if ($conn->connect_errno) {
   echo "Connection failed: ". $conn->connect_error . ".";
